9b3ce47c4e12a2ddcdf0b445ac32d225621dac58
RiccardoAntonini95 <riccardo.antonini95@gmail.com>
2024-07-24 12:23:21 +0200

    game board updating state

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+export default function GameBoard() {
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+    
+    function handleSelect(rowIndex, colIndex) {
+        setGameBoard((prevGameBoard) => {
+            const updatedGameBoard = [...prevGameBoard.map((innerArray) => [...innerArray])] //vanno copiati anche gli array interni
+            updatedGameBoard[rowIndex][colIndex] = 'X'
+            return updatedGameBoard;
+        })
+    }
+    
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => ( //array di array, prima le righe
+          <li key={rowIndex}>
+            <ol>
+              {row.map((playerSymbol,colIndex) => (//poi le colonne
+                  <li key={colIndex}>
+                    <button onClick={() => handleSelect(rowIndex, colIndex)}>{playerSymbol}</button>
+                  </li>
+                ))}
+            </ol>
+          </li>
+        ))}
+    </ol>
+  );
+}
